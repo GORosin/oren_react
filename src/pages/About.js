df6a91ef9efc736d923604bf10e320682b79baab
GORosin <oxr3231@g.rit.edu>
2021-09-18 09:57:47 -0400

    New Home background and About text

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,6 +3,11 @@ import React from 'react';
 function About(){
 	return(
 		<div className='about'>
+		<p>I am a  graduate of Rochester Institute of Technology with a B.Sc. in software engineering. I love the software development process, paticularly designing front end UIs. I have completed intenships in Alstom Signaling, Brookhaven National Lab, and Excelero. 
+		</p>
+	    <hr/>
+		<p>I collaborate well with others, and am comfortable working in a team environment or as an individual contributor. I’m a strong communicator and retain new information easily, and welcome the opportunity to develop my interpersonal skills further. I’m committed to doing the highest quality work, and ensure my projects are done correctly and on time.</p>
+<hr/>
 		</div>
 	)
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import guy_portrait from './oren_portrait.jpg';
+import oren_portrait from './oren_portrait.jpg';
 
 function Home(){
 	return(
 		<div className='home'>
 			<div className='textbox'>
-			<img src = {guy_portrait} id = "guy_portrait" alt = "guy"/>
+			<img src = {oren_portrait} id = "guy_portrait" alt = "oren"/>
 			<h1 className='home-textbox'>Oren Rosin</h1>
+			<h4>B.Sc Software Engineering</h4>
+			<h4>Full Stack Development</h4>
 			</div>
 		</div>
 	)
